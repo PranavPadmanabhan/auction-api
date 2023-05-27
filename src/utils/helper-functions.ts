a7423c0f7991938a42b3a4506e406a474f457049
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-05-27 16:05:49 +0530

    fifth commit

--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -16,21 +16,27 @@ export const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction
 
 
 export const ListenAuctions = async () => {
-   Rooms.find().then(async(rooms) => {
+   Rooms.find({}).then(async(rooms) => {
     console.log('total rooms : ',rooms.length)
     for (let i = 0; i < rooms.length; i++) {
       if (rooms[i].startingTime < Date.now() && rooms[i].endingTime > Date.now()) {
-        if (rooms[i].timestamp! - Date.now() >= 25000 || Date.now() - rooms[i].timestamp! >= 25000) {
+        const isEligible = rooms[i].timestamp! - Date.now() >= 25000 || Date.now() - rooms[i].timestamp! >= 25000;
+        console.log('eligibility - ',isEligible);
+        if (isEligible) {
           const nextIndex = rooms[i].activeBidderIndex! < rooms[i].participants.length - 1?rooms[i].activeBidderIndex! + 1 : 0;
           const nextParticipant = rooms[i].participants[nextIndex].userId;
           rooms[i].activeBidderIndex = nextIndex;
           rooms[i].activeBidder = nextParticipant;
-          rooms[i].timestamp = Date.now()
-          await rooms[i].save()
-          console.log("next user index : ",nextIndex)
+          rooms[i].timestamp = Date.now();
+          await rooms[i].save();
+          console.log("next user index : ",nextIndex);
         }
+        
       }
-
+      else{
+        await Rooms.findByIdAndDelete(rooms[i]._id)
+        console.log('deleted')
+      }
     }
   })
 
