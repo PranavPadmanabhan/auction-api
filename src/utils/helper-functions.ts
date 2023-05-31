27f713562d0d84ac3090487cdae15ef72d0175cb
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-05-31 09:22:01 +0530

    sixth commit

--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import Rooms from "../models/room";
 
 const apiKey = process.env.API_KEY;
 
@@ -15,32 +14,32 @@ export const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction
 // Apply the middleware to all routes
 
 
-export const ListenAuctions = async () => {
-   Rooms.find({}).then(async(rooms) => {
-    console.log('total rooms : ',rooms.length)
-    for (let i = 0; i < rooms.length; i++) {
-      if (rooms[i].startingTime < Date.now() && rooms[i].endingTime > Date.now()) {
-        const isEligible = rooms[i].timestamp! - Date.now() >= 25000 || Date.now() - rooms[i].timestamp! >= 25000;
-        console.log('eligibility - ',isEligible);
-        if (isEligible) {
-          const nextIndex = rooms[i].activeBidderIndex! < rooms[i].participants.length - 1?rooms[i].activeBidderIndex! + 1 : 0;
-          const nextParticipant = rooms[i].participants[nextIndex].userId;
-          rooms[i].activeBidderIndex = nextIndex;
-          rooms[i].activeBidder = nextParticipant;
-          rooms[i].timestamp = Date.now();
-          await rooms[i].save();
-          console.log("next user index : ",nextIndex);
-        }
+// export const ListenAuctions = async () => {
+//    Rooms.find({}).then(async(rooms) => {
+//     console.log('total rooms : ',rooms.length)
+//     for (let i = 0; i < rooms.length; i++) {
+//       if (rooms[i].startingTime < Date.now() && rooms[i].endingTime > Date.now()) {
+//         const isEligible = rooms[i].timestamp! - Date.now() >= 25000 || Date.now() - rooms[i].timestamp! >= 25000;
+//         console.log('eligibility - ',isEligible);
+//         if (isEligible) {
+//           const nextIndex = rooms[i].activeBidderIndex! < rooms[i].participants.length - 1?rooms[i].activeBidderIndex! + 1 : 0;
+//           const nextParticipant = rooms[i].participants[nextIndex].userId;
+//           rooms[i].activeBidderIndex = nextIndex;
+//           rooms[i].activeBidder = nextParticipant;
+//           rooms[i].timestamp = Date.now();
+//           await rooms[i].save();
+//           console.log("next user index : ",nextIndex);
+//         }
         
-      }
-      else{
-        await Rooms.findByIdAndDelete(rooms[i]._id)
-        console.log('deleted')
-      }
-    }
-  })
+//       }
+//       else{
+//         await Rooms.findByIdAndDelete(rooms[i]._id)
+//         console.log('deleted')
+//       }
+//     }
+//   })
 
-  setTimeout(() => {
-    ListenAuctions()
-  }, 5000);
-}
+//   setTimeout(() => {
+//     ListenAuctions()
+//   }, 5000);
+// }
